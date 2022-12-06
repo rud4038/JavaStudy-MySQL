32a8e33ba34bf74c149254b172ea155cadc675ad
rudtn4038 <mm453@naver.com>
2022-12-06 22:15:11 +0900

    insert

---
+++ insert.sql
@@ -0,0 +1,19 @@
+
+/*
+C(reate)R(ead)U(pdate)D(elete)
+
+INSERT (추가) - C
+SELECT (조회) - R
+UPDATE (수정) - U
+DELETE (삭제) - D
+대소문자 구분 안하고 쓰기때문에 전부다 스네이크 표기법으로 씀
+*/
+
+INSERT INTO product_mst
+(id, product_name,product_price) 
+VALUES
+(0, '스타벅스 블랙 머그컵3', 15000),
+(0, '스타벅스 화이트 머그컵3', 20000),
+(0, '스타벅스 블루 머그컵3', 25000);
+
+
